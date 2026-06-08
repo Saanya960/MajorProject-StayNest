3c8e06cb90fe21d8ae3bc1af7c96f9ec0abace02
Saanya Mittal <m.saanyak@gmail.com>
2026-06-08 15:08:50 +0530

    changed file name to index.js

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const app=express();
 const mongoose=require("mongoose");
 const ejs=require("ejs");
 
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1/stayNest")
+}
+
 main().then(() => {
 console.log("MONGODB Connected")
 })
 .catch((err) => {console.log(err)})
 
-
-async function main() {
-    await mongoose.connect("mongodb://127.0.0.1/wanderlust")
-}
-app.get("/",() => {
-    console.log("hi");
-})
-
 app.listen(8080,() => {
     console.log("app is working")
 })
+
+
+app.get("/",() => {
+    res.send(("hi"));
+})
+
