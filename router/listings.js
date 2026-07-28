670252c7690aeff31cd7d032963df48a303f5e25
Saanya Mittal <m.saanyak@gmail.com>
2026-07-28 20:47:57 +0530

    Add authentication middleware to protect routes and ensure users are logged in

--- router/listings.js
+++ router/listings.js
@@ -4,7 +4,8 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const { listingSchema } = require('../schema.js');
 const Listing = require('../models/listing.js');
-
+const passport = require('passport');
+const { isLoggedIn } = require('../middleware.js')
 const validateListing = (req,res,next) => {
 const {error} = listingSchema.validate(req.body);
     if(error) {
@@ -21,7 +22,7 @@ router.get('/',wrapAsync(async (req,res) => {
 }) );
 
 //Create Route
-router.get('/new' ,(req,res) => {
+router.get('/new' ,isLoggedIn,(req,res) => {
     res.render('listings/new.ejs');
 });
 
@@ -48,7 +49,9 @@ router.get('/:id',wrapAsync(async (req,res) =>
 }) );
 
 //Edit Route
-router.get('/:id/edit' ,wrapAsync(async (req,res) => {
+router.get('/:id/edit' ,
+    isLoggedIn,
+    wrapAsync(async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     if (!listing) {
@@ -61,6 +64,7 @@ router.get('/:id/edit' ,wrapAsync(async (req,res) => {
 
 //Update Route
 router.put('/:id' , 
+    isLoggedIn,
     validateListing,
     wrapAsync(async (req,res) => {
     if(!req.body.listing) {
@@ -74,7 +78,9 @@ router.put('/:id' ,
 }));
 
 //Delete Route
-router.delete('/:id' ,wrapAsync(async (req,res) => 
+router.delete('/:id' ,
+    isLoggedIn,
+    wrapAsync(async (req,res) => 
 {
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
