cf99b214c154456e924ab4adf64389f1881e6d2b
Saanya Mittal <m.saanyak@gmail.com>
2026-08-14 14:51:45 +0530

    Add validation middleware for listings and reviews; implement ownership checks for listings and authorship checks for reviews

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
+const Listing = require("./models/listing");
+const Review = require("./models/review");
+const { listingSchema, reviewSchema } = require('./schema.js');
+
+
+
 module.exports.isLoggedIn = (req,res,next) => {
     if(!req.isAuthenticated()) {
         req.session.redirectUrl=req.originalUrl;
@@ -13,3 +19,44 @@ module.exports.saveRedirectUrl = (req,res,next) => {
         }
         next();
 };
+
+module.exports.isOwner = async(req,res,next) => {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    if(!(listing && listing.owner._id.equals(req.user._id))) {
+        req.flash('error','you donot have permission to do that!');
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req,res,next) => {
+const {error} = listingSchema.validate(req.body);
+    if(error) {
+        const errMsg = error.details.map((el) => el.message).join(',');
+        req.flash('error',errMsg);
+
+        return res.redirect(req.get('Referer'));
+    } 
+        next();
+    };
+
+module.exports.validateReview = (req,res,next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const errMsg = error.details.map((el) => el.message).join(',');
+        req.flash('error',errMsg);
+        return res.redirect(req.get('Referer'));
+    } 
+        next();
+};
+
+module.exports.isAuthor = async(req,res,next) => {
+    const {reviewId, id} = req.params;
+    const review = await Review.findById(reviewId);
+    if(!(review && review.author.equals(req.user._id))) {
+        req.flash('error','you donot have permission to do that!');
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
