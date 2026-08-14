e3a37cf74499c00cc47e6c982317a68bc327051b
Saanya Mittal <m.saanyak@gmail.com>
2026-08-14 14:52:16 +0530

    Refactor review routes to use controller methods; remove inline validation and handling logic

--- router/reviews.js
+++ router/reviews.js
@@ -4,42 +4,22 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/ExpressError.js');
 const Listing = require('../models/listing.js');
 const Review = require('../models/review.js')
-const { reviewSchema } = require('../schema.js');
 
-const validateReview = (req,res,next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error) {
-        const errMsg = error.details.map((el) => el.message).join(',');
-        throw new ExpressError(400,errMsg);
-    } else {
-        next();
-    }
-}
+const {validateReview, isLoggedIn, isAuthor} = require('../middleware.js');
+
+const reviewController = require('../controllers/reviews.js');
+
 
 //reviews
 router.post('/' ,
+    isLoggedIn,
     validateReview,
-    wrapAsync(async (req,res) => {
-    let listing =await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash('success','Your review is created successfully!');
-
-    res.redirect(`/listings/${listing._id}`);
-}));
+    wrapAsync(reviewController.createReview));
 
 //Delete Review Route
-router.delete('/:reviewId' , wrapAsync(async (req,res) => {
-    let {id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate(id,{ $pull : {reviews : reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Your listing is deleted successfully!');
-
-    res.redirect(`/listings/${id}`);
-}))
+router.delete('/:reviewId' ,
+    isLoggedIn,
+    isAuthor,
+    wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
