75e1093a9fec3652dec6cdc1490c481a155e9caa
Saanya Mittal <m.saanyak@gmail.com>
2026-08-14 14:49:29 +0530

    Refactor user authentication routes to use controller methods and improve signup/login handling; update image rendering in listings to provide a default image

--- router/users.js
+++ router/users.js
@@ -5,50 +5,22 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const User = require('../models/user.js');
 const {saveRedirectUrl} = require('../middleware.js');
 
-router.get('/signup',(req,res) => {
-    res.render('listings/signup');
-});
+const userController = require('../controllers/users.js');
 
-router.post('/signup', wrapAsync( async (req,res) => {
-    try {
-        let {username,email,password} = req.body;
-        //to add a new user
-        const user1 = new User({username,email});
-        let newUser = await User.register(user1,password);
-        console.log(newUser);
-        req.login(newUser,(err) => {
-            if(err) {
-                return next(err);
-            }
-                req.flash('success','Welcome to StayNest!');
-                res.redirect('/listings');
-        })
-        
-        } catch(e) {
-            req.flash('error',e.message);
-            res.redirect('/signup');
-    }
-}));
+router.route('/signup')
+    .get(userController.renderSignupForm)
+    .post(wrapAsync( userController.signup));
 
-router.get('/login', (req,res) => {
-    res.render('listings/login');
-});
+router.route('/login')
+    .get(userController.renderLoginForm)
+    .post(
+        saveRedirectUrl,
+        passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+        wrapAsync(userController.login)
+    );
 
-router.post('/login',saveRedirectUrl,passport.authenticate('local', { failureRedirect: '/login', failureFlash: true, }), async(req,res) => {
-    req.flash('success','Welcome again!');
-    let redirectUrl = res.locals.redirectUrl || '/listings';
-    res.redirect(redirectUrl);
-})
 
-router.get('/logout', (req,res,next) => {
-    req.logout((err) => {
-        if(err) {
-            return next(err);
-        };
-        req.flash('success','You have logged out!');
-        res.redirect('/login');
-    });
-});
+router.get('/logout', userController.logout);
 
 
 
